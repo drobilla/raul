c507f80c17ef71e9d096c8c4bdd729c7adc68f6e
David Robillard <d@drobilla.net>
2010-01-05 03:50:10 +0000

    Add IntrusivePtr (trivial #define for boost::intrusive_ptr).
    git-svn-id: http://svn.drobilla.net/lad/trunk/raul@2340 a436a847-0d15-0410-975c-d299462d15a1

---
+++ raul/IntrusivePtr.hpp
@@ -0,0 +1,26 @@
+/* A "weak" pointer to a resource owned by a shared pointer.
+ * Copyright (C) 2007-2009 Dave Robillard <http://drobilla.net>
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef RAUL_INTRUSIVE_PTR_HPP
+#define RAUL_INTRUSIVE_PTR_HPP
+
+#include <boost/intrusive_ptr.hpp>
+
+#define IntrusivePtr boost::intrusive_ptr
+
+#endif // RAUL_INTRUSIVE_PTR_HPP
+
